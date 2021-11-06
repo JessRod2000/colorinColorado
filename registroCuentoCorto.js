8ae2462f1430bf75924a2abacd6feaca8b457dc1
JessicaRod <201801450@est.umss.edu>
2021-11-06 15:33:42 -0400

    se agrego el método que permita abrir el explorador de archivos y se muestre la imagen escogida en el formulario de registro

--- registroCuentoCorto.js
+++ registroCuentoCorto.js
@@ -1,1 +1,18 @@
-//lo llenas Jess :V
+
+var files = [];
+var reader;
+
+document.getElementById("elegirArchivo").onclick = function(e){
+    var input = document.createElement('input');
+    input.type='file';
+
+    input.onchange = e =>{
+        files = e.target.files;
+        reader  = new FileReader();
+        reader.onload = function(){
+            document.getElementById("myimg").src = reader.result;
+        }
+        reader.readAsDataURL(files[0]);
+    }
+    input.click();
+}
