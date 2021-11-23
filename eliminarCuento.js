57cce6fb2e7838cf9d8775181bb57ba52038505a
nicolecg <201902485@est.umss.edu>
2021-11-23 09:13:47 -0400

    arreglo de bug eliminar, notifcacion de cuento no registrado

--- eliminarCuento.js
+++ eliminarCuento.js
@@ -1,48 +1,115 @@
 const db = firebase.firestore();
 
-const getCuentosFabula = () => db.collection('Fabulas').get();
-const getCuentosClasicos = () => db.collection('Clasicos').get();
-const getCuentosFantasia = () => db.collection('Fantasia').get();
- 
-var titulospan = document.getElementById('text1'); 
-botonEliminar.onclick = async(e) =>{
-      const coleccionFabulas = await getCuentosFabula();
-      const coleccionFantasia = await getCuentosFantasia();
-      const coleccionClasicos = await getCuentosClasicos();
-      cuentoEliminado = document.getElementById('cuentobox').value;
-      var categoria = document.getElementById('categorias').value;
-        botonEliminar = document.getElementById('botonEliminar');
-        console.log(cuentoEliminado);
-        if(cuentoEliminado!= ""){
-           if(categoria=="Fantasia"){
-            coleccionFantasia.forEach(doc =>{
-             if (doc.data().TituloCuento==cuentoEliminado){
+var controlar = "true";
+var categoria,titulobox;
+
+document.getElementById("form").addEventListener("submit", function(event){
+   event.preventDefault();
+   titulobox = document.getElementById("cuentobox").value;
+   categoria = document.getElementById("categorias").value;
+
+    verificarExistencia();
+   
+   document.getElementById("form").reset();
+})
+//-----------------ELIMINAR------------------------------------
+function eliminar(){
+    return new Promise((resolve,reject)=>{
+    if(categoria == "Fantasia"){
+        dbfirestore.collection('Fantasia').get().then((snapshot)=>{
+           snapshot.docs.forEach(doc=>{
+               if(titulobox == doc.data().TituloCuento){
+                alert("Cuento eliminado de la colección Fantasía");
                 doc.ref.delete();
-                alert("Cuento borrado de la categoría Fantasía");
-             }
-        });
-        }else{
-           if(categoria=="Clasicos"){
-            coleccionClasicos.forEach(doc =>{
-                 if (doc.data().TituloCuento==cuentoEliminado){
+               }
+
+           })
+           
+       })
+       setTimeout(()=>{
+        resolve();
+        ;} , 1000);
+     }else{
+         if(categoria == "Fabulas"){
+           dbfirestore.collection('Fabulas').get().then((snapshot)=>{
+              snapshot.docs.forEach(doc=>{
+                  if(titulobox == doc.data().TituloCuento){
+                    alert("Cuento eliminado de la colección Fábulas");
                     doc.ref.delete();
-                    alert("Cuento borrado de la categoría Clásicos");
-                 }
-            });
-            }else{
-               if(categoria=="Fabulas"){
-                coleccionFabulas.forEach(doc =>{
-                     if (doc.data().TituloCuento==cuentoEliminado){
-                        doc.ref.delete();
-                        alert("Cuento borrado de la categoría Fábulas");
-                     }
-                 })
-              }
-            }
+                  }
+              })
+          })
+          setTimeout(()=>{
+            resolve();
+            ;} , 1000);
+         }else{
+           dbfirestore.collection('Clasicos').get().then((snapshot)=>{
+              snapshot.docs.forEach(doc=>{
+                  if(titulobox == doc.data().TituloCuento){
+                    alert("Cuento eliminado de la colección Clásicos");
+                    doc.ref.delete();
+                  }
+              })
+              
+          })
+          setTimeout(()=>{
+            resolve();
+            ;} , 1000);
          }
-          
-                document.getElementById("form").reset();
-        }else{
-            titulospan.innerHTML="Ingrese titulo del cuento";
+         
+     }
+    });
+}
+//------------------------------------
+async function verificarExistencia(){
+    await testTitulos();
+if (controlar == "true" ) {
+    alert('El cuento no se encuentra registrado');
+}else {
+        await eliminar();
+        controlar = "true";
+    }
+}
+//-----revisar si existe el cuento-------
+function testTitulos(){
+return new Promise((resolve,reject)=>{
+    var titulo = document.getElementById("cuentobox").value;
+        dbfirestore.collection('Fabulas').get().then((snapshot)=>{
+            snapshot.docs.forEach(doc=>{
+                if(titulo == doc.data().TituloCuento){
+                    controlar = "false";
+                }
+            }
+            );
+            
+    setTimeout(()=>{
+        resolve();
+        ;} , 1000);
+    });
+    dbfirestore.collection('Fantasia').get().then((snapshot)=>{
+        snapshot.docs.forEach(doc=>{
+            if(titulo == doc.data().TituloCuento){
+                controlar = "false";
+            }
         }
-    } 
+        );
+        
+setTimeout(()=>{
+    resolve();
+    ;} , 1000);
+});
+dbfirestore.collection('Clasicos').get().then((snapshot)=>{
+    snapshot.docs.forEach(doc=>{
+        if(titulo == doc.data().TituloCuento){
+            controlar = "false";
+        }
+    }
+    );
+setTimeout(()=>{
+resolve();
+;} , 1000);
+});
+
+});
+
+}
